7b4581a77b10c29aabc5f27255d304b8c71cd825
Khaldon Naif <khaldonnaif@gmail.com>
2025-06-03 02:28:01 +0800

    Add About Page

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
@@ -20,9 +25,15 @@ app.get("/result", async (req, res) => {
         res.render("result.ejs", { data: response.data });
     } catch(error) {
         console.log(`Error message: ${error}`);
-        res.sendStatus(404);
+        res
+        .sendStatus(404)
+        .send("No data was found. Make sure the coordinates you sent are within the UK.");
     }
 });
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname+"/public/about.html");
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
